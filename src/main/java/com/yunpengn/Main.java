9a2642642c3003d6c0ba0c4d1a4b14835acb71d1
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-01-22 22:39:13 +0800

    Setup project

---
+++ src/main/java/com/yunpengn/Main.java
@@ -0,0 +1,7 @@
+package com.yunpengn;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, world! We love CS4225 @ NUS SoC.");
+  }
+}
---
+++ src/test/java/com/yunpengn/MainTest.java
@@ -0,0 +1,11 @@
+package com.yunpengn;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+  @Test
+  void test() {
+    String[] args = new String[0];
+    Main.main(args);
+  }
+}
